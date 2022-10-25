42ffb80f81c1f8a0a604eeed4557e0b95eea05f1
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-10-25 20:40:45 +0100

    added psycopg2 to requirements.txt

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'pass',
+        'PASSWORD': 'vrag',
     }
 }
 
