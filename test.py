7213110d2e5719a90a248539260da1be67bd0abf
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-12-16 16:47:22 +0000

    added test.py

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
+
 # from logistic.models import Product
 
 
 class TestLogic(TestCase):
+    def test_view(self):
+        client = Client()
+        response = client.get('/test/')
+        self.assertEqual(response.content.decode(), "Test done!")
+
     def test_2_2(self):
-        self.assertEqual(2,  2)
+        self.assertEqual(2, 2)
